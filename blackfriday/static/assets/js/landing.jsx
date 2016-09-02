39cb84e236e42c774d1a273cd0afcba4b9c07ad9
Vladimir Rodkin <rodkin.vladimir@220-volt.ru>
2016-09-02 16:11:24 +0300

    BF-18 Updated landing page makeup

--- blackfriday/static/assets/js/landing.jsx
+++ blackfriday/static/assets/js/landing.jsx
@@ -1,1 +1,7 @@
 require('css/landing.styl');
+
+(function () {
+	require('./landing/anchors.js');
+	require('./landing/carousels.js');
+	require('./landing/countdown.js');
+})();
---
+++ blackfriday/static/assets/js/landing/carousels.js
@@ -0,0 +1,19 @@
+/* global $ */
+
+// Включаем карусель партнеров когда их больше 1 страницы
+if ($('.bxslider-one > div').length > 1) {
+	$('.bxslider-one').bxSlider({
+		minSlides: 1,
+		maxSlides: 1,
+		auto: true
+	});
+}
+
+$('.bxslider-multi').bxSlider({
+	slideWidth: 182,
+	minSlides: 6,
+	maxSlides: 6,
+	moveSlides: 1,
+	auto: true,
+	pager: false
+});
