f6bf7b6b9bb64b0d8fdfee7e75058d6e1ae479db
Artem Suhanov <sukhanov@220-volt.ru>
2016-09-02 17:43:58 +0300

    BF-39 camelCase for forms

---
+++ blackfriday/libs/api/parsers.py
@@ -0,0 +1,31 @@
+from djangorestframework_camel_case.util import underscoreize
+from rest_framework.parsers import MultiPartParser, FormParser, DataAndFiles
+
+
+def underscore_data_and_files(data_and_files):
+    data = underscoreize(data_and_files.data)
+    files = underscoreize(data_and_files.files)
+
+    for data_dict in [data, files]:
+        for key in data_dict:
+            if data_dict[key] == 'null':
+                data_dict[key] = None
+
+    return DataAndFiles(data=data, files=files)
+
+
+class CamelCaseMultiPartParser(MultiPartParser):
+    def parse(self, stream, media_type=None, parser_context=None):
+        data = super().parse(stream, media_type, parser_context)
+        return underscore_data_and_files(data)
+
+
+class CamelCaseFormParser(FormParser):
+    def parse(self, stream, media_type=None, parser_context=None):
+        data = super().parse(stream, media_type, parser_context)
+
+        for key in data:
+            if data[key] == 'null':
+                data[key] = None
+
+        return data
--- blackfriday/settings/api.py
+++ blackfriday/settings/api.py
@@ -5,8 +5,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser',
+        'libs.api.parsers.CamelCaseFormParser',
+        'libs.api.parsers.CamelCaseMultiPartParser',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
