e75842846bc6b97ac45e6dd91a9b3179a7a9e659
Vladimir Rodkin <rodkin.vladimir@220-volt.ru>
2016-09-02 17:12:56 +0300

    BF-18 Added landing subscribe modal

--- blackfriday/static/assets/js/landing/forms.js
+++ blackfriday/static/assets/js/landing/forms.js
@@ -1,3 +1,21 @@
-/* global $ */
+/* global $ toastr */
 
-$('form').ajaxForm();
+import Cookie from 'js-cookie';
+
+const csrftoken = Cookie.get('csrftoken');
+
+$.ajaxSetup({
+	headers: {
+		'X-CSRFToken': csrftoken
+	}
+});
+
+$('form').ajaxForm({
+	clearForm: true,
+	success: showResponse
+});
+
+function showResponse() {
+	toastr.success('Заявка успешно отправлена');
+	$('.modal.in').modal('hide');
+}
