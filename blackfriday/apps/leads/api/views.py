e625878f9f354c8a4ee15bf9428558608cf3e9b5
Artem Suhanov <sukhanov@220-volt.ru>
2016-09-05 11:52:11 +0300

    BF-31 API subscribers fixes

--- blackfriday/apps/leads/api/views.py
+++ blackfriday/apps/leads/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
 
 from libs.api.permissions import action_permission, IsAuthenticated, IsManager, IsAdmin
 from .serializers import Subscriber, SubscriberSerializer
@@ -13,3 +14,10 @@ class SubscribersViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.
     ]
     serializer_class = SubscriberSerializer
     queryset = Subscriber.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        subscriber = Subscriber.objects.filter(email=request.data.get('email')).first()
+        serializer = self.get_serializer(instance=subscriber, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK if subscriber else status.HTTP_201_CREATED)
