6aa0fd8055299cace21e2b4779db7e19a1ecf8b9
Artem Suhanov <sukhanov@220-volt.ru>
2016-09-05 15:49:42 +0300

    BF-40 send lead report

---
+++ blackfriday/apps/leads/management/commands/send_lead_report.py
@@ -0,0 +1,31 @@
+import codecs
+import csv
+import io
+
+from django.conf import settings
+from django.core.mail.message import EmailMessage
+from django.core.management import BaseCommand
+from django.utils.datetime_safe import datetime
+
+from ...models import AdvertiserRequest
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        fieldnames = ['name', 'organization_name', 'email', 'phone']
+        qs = AdvertiserRequest.objects.all().order_by('-datetime_updated').values(*fieldnames)
+
+        csv_file = io.StringIO()
+        csv_file.write(codecs.BOM_UTF8.decode('utf-8'))
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=';')
+        csv_writer.writeheader()
+        for row in qs:
+            csv_writer.writerow(row)
+
+        message = EmailMessage(
+            subject='Отчет по заявкам',
+            body='Сгенерирован {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+            to=map(lambda x: '"{}" <{}>'.format(*x), settings.MANAGERS)
+        )
+        message.attach(filename='leads.csv', content=csv_file.getvalue(), mimetype='text/csv')
+        message.send()
