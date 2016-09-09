93874499badf0b02d4a202c4c9876284bff09754
Artem Suhanov <sukhanov@220-volt.ru>
2016-09-09 15:36:27 +0300

    BF-64 registration api

--- blackfriday/apps/users/api/serializers.py
+++ blackfriday/apps/users/api/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.hashers import make_password
+from libs.recaptcha import recaptcha
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from ..models import User
 
@@ -25,3 +27,24 @@ class UserUpdateSerializer(UserSerializer):
 
     def to_representation(self, instance):
         return UserSerializer().to_representation(instance)
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    captcha = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ('name', 'email', 'password', 'captcha')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initial_data['captcha'] = self.initial_data.pop('g-recaptcha-response', None)
+
+    def validate_password(self, value):
+        return make_password(value)
+
+    def validate_captcha(self, value):
+        request = self.context['request']
+        if not recaptcha.is_valid(request, value):
+            raise ValidationError('Невалидная капча')
+        return value
--- blackfriday/apps/users/api/views.py
+++ blackfriday/apps/users/api/views.py
@@ -1,8 +1,11 @@
 from smtplib import SMTPException
 
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.urls import reverse
 
+from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
@@ -10,7 +13,33 @@ from rest_framework.response import Response
 from libs.api.exceptions import ServiceUnavailable
 from libs.api.permissions import IsAdmin, IsAuthenticated
 
-from .serializers import User, UserSerializer, UserUpdateSerializer
+from ..models import Token, TokenType
+from .serializers import User, UserSerializer, UserUpdateSerializer, RegistrationSerializer
+
+
+def send_verification(request, user):
+    Token.invalidate(user)
+    token = Token.create(user, type=TokenType.VERIFICATION, ttl=settings.VERIFICATION_TTL_HOURS)
+
+    message = render_to_string(
+        'users/messages/verification.txt',
+        context={
+            'user': user,
+            'token': token,
+            'host': request.get_host(),
+            'path': reverse('users:verification'),
+            'scheme': request.scheme,
+            'hours': '{} час{}'.format(
+                settings.VERIFICATION_TTL_HOURS,
+                'а' if settings.VERIFICATION_TTL_HOURS % 10 == 1 else 'ов'
+            )
+        }
+    )
+
+    try:
+        send_mail(message=message, recipient_list=[user.email], **settings.VERIFICATION)
+    except SMTPException:
+        raise ServiceUnavailable('Ошибка отправки почты')
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -27,18 +56,14 @@ class UserViewSet(viewsets.ModelViewSet):
     @detail_route(methods=['post'])
     def verification(self, request, *args, **kwargs):
         instance = self.get_object()
-        context = {
-            'host': request.get_host(),
-            'path': reverse('users:verification'),
-            'scheme': request.scheme,
-            'hours': '{} час{}'.format(
-                settings.VERIFICATION_TTL_HOURS,
-                'а' if settings.VERIFICATION_TTL_HOURS % 10 == 1 else 'ов'
-            )
-        }
-
-        try:
-            instance.send_verification(context)
-        except SMTPException:
-            raise ServiceUnavailable('Ошибка отправки почты')
+        send_verification(request, instance)
         return Response(self.get_serializer(instance).data)
+
+
+class RegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegistrationSerializer
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        send_verification(self.request, instance)
