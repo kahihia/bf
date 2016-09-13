eeaec2bbb7d12ca817cbbd23cd29ce09ecd027a3
Vladimir Rodkin <rodkin.vladimir@220-volt.ru>
2016-09-13 11:19:26 +0300

    BF-80 Refactored Form control components

---
+++ blackfriday/static/assets/js/admin/components/input.jsx
@@ -0,0 +1,130 @@
+/* eslint react/require-optimization: 0 */
+
+import React from 'react';
+import MaskedInput from 'react-maskedinput';
+import Select from './select.jsx';
+
+class Input extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleKeyUp = this.handleKeyUp.bind(this);
+	}
+
+	handleChange(e) {
+		let ev = e;
+
+		if (this.props.type === 'select') {
+			ev = {
+				target: {
+					name: this.props.name,
+					value: e
+				}
+			};
+		}
+
+		this.props.onChange(ev);
+	}
+
+	handleKeyUp(e) {
+		if (!this.props.onKeyUp) {
+			return;
+		}
+		this.props.onKeyUp(e);
+	}
+
+	render() {
+		const {
+			addon,
+			mask,
+			name,
+			options,
+			placeholder,
+			readOnly,
+			required,
+			type,
+			value,
+			valueType
+		} = this.props;
+
+		let val = value;
+		if (value === null) {
+			if (valueType === 'Number') {
+				val = 0;
+			} else {
+				val = '';
+			}
+		}
+
+		let input;
+		if (type === 'select') {
+			input = (
+				<Select
+					onChange={this.handleChange}
+					selected={val}
+					{...{options, name, valueType}}
+					/>
+			);
+		} else if (mask) {
+			input = (
+				<MaskedInput
+					className="form-control"
+					value={val}
+					{...{name, required, readOnly, mask}}
+					onChange={this.handleChange}
+					onKeyUp={this.handleKeyUp}
+					/>
+			);
+		} else {
+			input = (
+				<input
+					className="form-control"
+					value={val}
+					{...{placeholder, name, type, required, readOnly}}
+					onChange={this.handleChange}
+					onKeyUp={this.handleKeyUp}
+					/>
+			);
+		}
+
+		if (addon) {
+			return (
+				<div className="input-group">
+					<span className="input-group-addon">
+						{addon}
+					</span>
+					{input}
+				</div>
+			);
+		}
+
+		return input;
+	}
+}
+Input.propTypes = {
+	addon: React.PropTypes.string,
+	mask: React.PropTypes.string,
+	name: React.PropTypes.string,
+	onChange: React.PropTypes.func.isRequired,
+	onKeyUp: React.PropTypes.func,
+	options: React.PropTypes.oneOfType([
+		React.PropTypes.array,
+		React.PropTypes.object
+	]),
+	placeholder: React.PropTypes.string,
+	readOnly: React.PropTypes.bool,
+	required: React.PropTypes.bool,
+	type: React.PropTypes.string,
+	value: React.PropTypes.oneOfType([
+		React.PropTypes.string,
+		React.PropTypes.number
+	]),
+	valueType: React.PropTypes.string
+};
+Input.defaultProps = {
+	readOnly: false,
+	required: false,
+	type: 'text'
+};
+
+export default Input;
