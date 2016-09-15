1190aef475a337b72f7e2a51c38ce7fa88aae17a
nmerkulov <merkulov.nikita@220-volt.ru>
2016-09-15 13:47:05 +0300

    BF-55 added tests for readers

--- blackfriday/settings/testing.py
+++ blackfriday/settings/testing.py
@@ -1,1 +1,15 @@
 from . import *
+
+PRODUCT_FILE_COLUMNS_MAPPING = {
+    'name': 'name',
+    'oldprice': 'old_price',
+    'price': 'price',
+    'discount': 'discount',
+    'startprice': 'start_price',
+    'currencyid': 'currency',
+    'vendor': 'brand',
+    'category': 'category',
+    'countryoforigin': 'country',
+    'url': 'url',
+    'image': 'image',
+}
