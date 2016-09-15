1a0e910298ec9568e2c9230b64ce431f54f518f3
Artem Suhanov <sukhanov@220-volt.ru>
2016-09-15 19:00:16 +0300

    BF-88 profile page

--- blackfriday/apps/advertisers/urls.py
+++ blackfriday/apps/advertisers/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from apps.advertisers.views import MerchantDetailView, ModerationListView
 
-from .views import AdvertiserListView, MerchantListView
+from .views import AdvertiserListView, MerchantListView, ProfileView
 
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^merchants/$', MerchantListView.as_view(), name='merchants-list'),
     url(r'^merchants/(?P<pk>\d+)/$', MerchantDetailView.as_view(), name='merchant-details'),
     url(r'^moderation/$', ModerationListView.as_view(), name='moderation-list'),
+    url(r'^profile/$', ProfileView.as_view(), name='profile')
 ]
