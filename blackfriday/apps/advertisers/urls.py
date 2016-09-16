1fc908f806b00f25881257b0bb88e879ccc044d9
Artem Suhanov <sukhanov@220-volt.ru>
2016-09-16 10:56:11 +0300

    HOTFIX missed rename in urls.py

--- blackfriday/apps/advertisers/urls.py
+++ blackfriday/apps/advertisers/urls.py
@@ -5,8 +5,8 @@ from .views import AdvertiserListView, MerchantListView
 
 
 urlpatterns = [
-    url(r'^advertisers/$', AdvertiserListView.as_view(), name='advertisers-list'),
-    url(r'^merchants/$', MerchantListView.as_view(), name='merchants-list'),
+    url(r'^advertisers/$', AdvertiserListView.as_view(), name='advertiser-list'),
+    url(r'^merchants/$', MerchantListView.as_view(), name='merchant-list'),
     url(r'^merchants/(?P<pk>\d+)/$', MerchantDetailView.as_view(), name='merchant-details'),
     url(r'^moderation/$', ModerationListView.as_view(), name='moderation-list'),
 ]
