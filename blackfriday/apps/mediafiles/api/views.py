c0b0f00980c1025e2626985707a060d640878e98
nmerkulov <merkulov.nikita@220-volt.ru>
2016-09-20 15:31:24 +0300

    BF-100 removed json parsing

--- blackfriday/apps/mediafiles/api/views.py
+++ blackfriday/apps/mediafiles/api/views.py
@@ -4,9 +4,11 @@ from libs.api.permissions import IsAuthenticated
 
 from .serializers import ImageSerializer
 from ..models import Image
+from libs.api.parsers import CamelCaseFormParser, CamelCaseMultiPartParser
 
 
 class ImageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [CamelCaseFormParser, CamelCaseMultiPartParser]
