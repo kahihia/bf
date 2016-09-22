e92f0b99eaf232b876158fccad89f11353c16c62
Artem Suhanov <sukhanov@220-volt.ru>
2016-09-22 13:22:36 +0300

    BF-122 data migration

--- blackfriday/apps/users/migrations/0004_auto_20160922_0922.py
+++ blackfriday/apps/users/migrations/0004_auto_20160922_0922.py
@@ -5,6 +5,22 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 
+def migrate_role(apps, schema_editor):
+    User = apps.get_model('users', 'User')
+    for user in User.objects.all():
+        if user.is_admin:
+            user._role = 3
+        elif user.profile:
+            user._role = 2
+        else:
+            continue
+        user.save()
+
+
+def noop(apps, schema_editor):
+    pass
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -12,13 +28,14 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='user',
-            name='is_admin',
-        ),
         migrations.AddField(
             model_name='user',
             name='_role',
             field=models.IntegerField(choices=[(0, 'operator'), (1, 'manager'), (2, 'advertiser'), (3, 'admin')], default=1),
+        ),
+        migrations.RunPython(migrate_role, noop),
+        migrations.RemoveField(
+            model_name='user',
+            name='is_admin',
         ),
     ]
