bb231da620f5c7a2d7eef49420f29a69033ed2b5
VictorSmirnov-220Volt <vsmirnov@220-volt.ru>
2016-09-30 14:23:33 +0300

    HOTFIX. Added default ordering by position for landing logos

--- blackfriday/apps/landing/models.py
+++ blackfriday/apps/landing/models.py
@@ -7,3 +7,6 @@ class LandingLogo(models.Model):
     image = ResizedImageField(upload_to='landing-logos', quality=100, size=[210, 130], verbose_name='Изображение')
     url = models.URLField(verbose_name='URL')
     position = models.PositiveSmallIntegerField(null=True)
+
+    class Meta:
+        ordering = ['position']
