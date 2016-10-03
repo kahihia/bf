e0402411ac63b39330b2c133f7e3e9727e86f238
nmerkulov <merkulov.nikita@220-volt.ru>
2016-10-03 10:45:42 +0300

    BF-153 fixed role checks

--- blackfriday/apps/users/mixins.py
+++ blackfriday/apps/users/mixins.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from apps.users.models import Role
 
 
 class ManagerOrAdminOnlyMixin(UserPassesTestMixin):
@@ -20,4 +19,4 @@ class ManagerOrAdminOrAdvertiser(UserPassesTestMixin):
     raise_exception = True
 
     def test_func(self):
-        return self.request.user.role in [Role.MANAGER, Role.ADVERTISER, Role.ADMIN]
+        return self.request.user.role in ('manager', 'admin', 'advertiser')
