613d41238b3c00a384478d4ba60a7f4117b05bb3
Artem Suhanov <sukhanov@220-volt.ru>
2016-10-07 12:16:16 +0300

    BF-173 fixed permissions

--- blackfriday/apps/specials/views.py
+++ blackfriday/apps/specials/views.py
@@ -3,4 +3,8 @@ from django.views.generic import TemplateView
 
 
 class SpecialListView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    raise_exception = True
     template_name = 'specials/special-list.html'
+
+    def test_func(self):
+        return self.request.user.role in ('admin', 'advertiser')
