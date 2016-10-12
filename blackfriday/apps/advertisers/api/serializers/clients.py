59cfb3ceff0c4d7ee0c4b7d4ceac3b9327c7d9e8
Artem Suhanov <sukhanov@220-volt.ru>
2016-10-12 16:09:51 +0300

    BF-208 options_count in merchant list

--- blackfriday/apps/advertisers/api/serializers/clients.py
+++ blackfriday/apps/advertisers/api/serializers/clients.py
@@ -178,7 +178,7 @@ class MerchantListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Merchant
         fields = ('id', 'name', 'image', 'payment_status', 'moderation', 'promo',
-                  'is_active', 'is_previewable', 'preview_url', 'advertiser')
+                  'is_active', 'is_previewable', 'preview_url', 'advertiser', 'options_count')
 
     def get_moderation(self, obj):
         return MerchantModerationSerializer(obj).data
