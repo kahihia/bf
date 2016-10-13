bca1a57d657dbd30b1632e1ead761761b8642091
Artem Suhanov <sukhanov@220-volt.ru>
2016-10-13 14:28:13 +0300

    BF-212 filter for category api

--- blackfriday/apps/catalog/api/views.py
+++ blackfriday/apps/catalog/api/views.py
@@ -23,6 +23,7 @@ from ..models import Product, Category
 from ..feeds.verifier import FeedParser
 from ..feeds.generator import FeedGenerator
 
+from .filters import CategoryFilter
 from .serializers import CategorySerializer, ProductSerializer
 
 
@@ -30,6 +31,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated, IsAdmin | ReadOnly]
+    filter_class = CategoryFilter
 
     def get_queryset(self):
         qs = super().get_queryset()
