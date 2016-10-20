9d33f4ecc852e932ce22f5f647a0e4319aeb2e1e
VictorSmirnov-220Volt <vsmirnov@220-volt.ru>
2016-10-20 20:22:16 +0300

    BF-224. Added views for logos and banners mailing pages

--- blackfriday/apps/mailing/urls.py
+++ blackfriday/apps/mailing/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from .views import MailingView
+from .views import LogosMailingView, BannersMailingView, GoToMailingView
 
 urlpatterns = [
-    url(r'^mailing/$', MailingView.as_view()),
+    url(r'^mailing/$', GoToMailingView.as_view()),
+    url(r'^mailing/logos/$', LogosMailingView.as_view(), name='logos'),
+    url(r'^mailing/banners/$', BannersMailingView.as_view(), name='banners'),
 ]
