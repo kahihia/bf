0366c7b305def3864113c0ffaf06826ee4231923
nmerkulov <merkulov.nikita@220-volt.ru>
2016-10-28 10:05:11 +0300

    BF-166 replaced paymentservice by pysberbps

--- blackfriday/apps/payment/api/serializers.py
+++ blackfriday/apps/payment/api/serializers.py
@@ -1,6 +1,8 @@
 import logging
 
+from pysberbps import SberRequestError
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from apps.orders.models import Invoice
 from apps.payment.models import Payment
@@ -10,17 +12,31 @@ logger = logging.getLogger(__name__)
 
 
 class PaymentCreateSerializer(serializers.ModelSerializer):
-    invoice_id = serializers.PrimaryKeyRelatedField(queryset=Invoice.objects.all(), source='invoice')
+    invoice_id = serializers.PrimaryKeyRelatedField(
+        queryset=Invoice.objects.filter(payment__isnull=True), source='invoice',
+        error_messages={'does_not_exist': 'Недопустимый первичный ключ'}
+    )
     form_url = serializers.CharField(read_only=True)
 
     class Meta:
         model = Payment
         fields = ('form_url', 'invoice_id')
 
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except SberRequestError as e:
+            raise ValidationError(
+                {
+                    'code': e.code,
+                    'message': e.desc
+                }
+            )
+
 
 class PaymentSerializer(serializers.ModelSerializer):
-    status = serializers.CharField(read_only=True)
-    message = serializers.CharField(read_only=True)
+    status = serializers.CharField()
+    message = serializers.CharField()
 
     class Meta:
         model = Payment
