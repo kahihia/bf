d7b018a2e4b35472ad78f3075a91ac40f6440cb4
Vladimir Rodkin <rodkin.vladimir@220-volt.ru>
2016-11-02 16:33:45 +0300

    Removed unused components

--- blackfriday/static/assets/js/admin.jsx
+++ blackfriday/static/assets/js/admin.jsx
@@ -1,19 +1,2 @@
-/* global window document */
-
 require('css/admin.styl');
 require('node_modules/react-datepicker/dist/react-datepicker.css');
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-(function () {
-	'use strict';
-
-	const merchantLimitsWarning = document.getElementById('merchant-limits-warning');
-	if (merchantLimitsWarning) {
-		let MerchantLimitsWarning = require('./admin/advertiser/merchant-limits-warning');
-		window.renderMerchantLimitsWarning = function (cb) {
-			ReactDOM.render(<MerchantLimitsWarning success={cb}/>, merchantLimitsWarning);
-		};
-	}
-})();
