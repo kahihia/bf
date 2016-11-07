d8a450b7ced473ff1fcf1e2f71f6d9c231da64d0
nmerkulov <merkulov.nikita@220-volt.ru>
2016-11-07 15:48:00 +0300

    BF-310 new files uploaded now unique name

--- blackfriday/apps/mediafiles/models.py
+++ blackfriday/apps/mediafiles/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from libs.db.fields import upload_to
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=upload_to('images/'))
 
     def __str__(self):
         return self.image.url
