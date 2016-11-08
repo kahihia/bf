c7dbb358a9d9a315486ffeb58aab5ca4a125a16e
nmerkulov <merkulov.nikita@220-volt.ru>
2016-11-08 17:09:25 +0300

    HOTFIX removed uuiding uploaded paths

--- blackfriday/apps/landing/models.py
+++ blackfriday/apps/landing/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-from libs.db.fields import ResizedImageField, upload_to
+from libs.db.fields import ResizedImageField
 
 
 class LandingLogo(models.Model):
     image = ResizedImageField(
-        upload_to=upload_to('landing-logos'), quality=100, size=[210, 130], verbose_name='Изображение')
+        upload_to='landing-logos', quality=100, size=[210, 130], verbose_name='Изображение')
     url = models.URLField(verbose_name='URL')
     position = models.PositiveSmallIntegerField(null=True)
 
