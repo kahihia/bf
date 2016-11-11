3aab28d908166079ad9de2f92d2c51d72a4d4236
nmerkulov <merkulov.nikita@220-volt.ru>
2016-11-11 10:58:49 +0300

    HOTFIX copypaste login without csrf

--- blackfriday/apps/users/urls.py
+++ blackfriday/apps/users/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
 
-from .views import UserListView, VerificationView, RedirectByRoleView
-
+from .views import UserListView, VerificationView, RedirectByRoleView, login_no_csrf
 
 urlpatterns = [
     url(r'^$', RedirectByRoleView.as_view()),
@@ -11,7 +10,7 @@ urlpatterns = [
     url(r'^registration/$', TemplateView.as_view(template_name='users/registration.html')),
     url(r'^verification/$', VerificationView.as_view(), name='verification'),
 
-    url(r'^login/$', auth_views.login, {'template_name': 'users/login.html', 'redirect_authenticated_user': True}),
+    url(r'^login/$', login_no_csrf, {'template_name': 'users/login.html', 'redirect_authenticated_user': True}),
     url(r'^logout/$', auth_views.logout_then_login, name='logout'),
 
     url(r'^users/$', UserListView.as_view(), name='user-list'),
