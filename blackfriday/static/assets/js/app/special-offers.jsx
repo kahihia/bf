811725d2e15cfad3e42ce423c844f835453f146a
Vladimir Rodkin <rodkin.vladimir@220-volt.ru>
2016-11-15 12:10:16 +0300

    BF-317 Added app merchant page action banners responsive carousel

--- blackfriday/static/assets/js/app/special-offers.jsx
+++ blackfriday/static/assets/js/app/special-offers.jsx
@@ -1,29 +1,57 @@
+/* global window */
+
 import React from 'react';
+import Slider from 'react-slick';
 import Link from './link.jsx';
+
+const settings = {
+	infinite: false,
+	draggable: false,
+	swipe: false,
+	speed: 500,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+	dots: true,
+	responsive: [
+		{
+			breakpoint: 650,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
+};
+
+const renderSpecialOffersList = data => data.map(banner => (
+	<div
+		key={banner.id}
+		className="col-sm-6"
+		>
+		<div className="special-offers__item">
+			<Link
+				href={banner.url}
+				className="special-offers__link embed-responsive"
+				isExternal
+				>
+				<img
+					src={banner.image}
+					alt=""
+					className="special-offers__banner embed-responsive-item"
+					/>
+			</Link>
+		</div>
+	</div>
+));
 
 const SpecialOffers = props => (
 	<div className="special-offers">
 		<div className="row">
-			{props.data.map(banner => (
-				<div
-					key={banner.id}
-					className="col-sm-6"
-					>
-					<div className="special-offers__item">
-						<Link
-							href={banner.url}
-							className="special-offers__link embed-responsive"
-							isExternal
-							>
-							<img
-								src={banner.image}
-								alt=""
-								className="special-offers__banner embed-responsive-item"
-								/>
-						</Link>
-					</div>
-				</div>
-			))}
+			{window.innerWidth > 949 ? renderSpecialOffersList(props.data) : (
+				<Slider {...settings}>
+					{renderSpecialOffersList(props.data)}
+				</Slider>
+			)}
 		</div>
 	</div>
 );
