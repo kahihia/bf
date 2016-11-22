1235777e111cbef121b63910ce56ad768fbfc546
Vladimir Shkoda <shkoda.vladimir@220-volt.ru>
2016-11-22 13:18:54 +0300

    BF-239 Added management command for merchant logs

--- blackfriday/apps/reports/models.py
+++ blackfriday/apps/reports/models.py
@@ -9,17 +9,29 @@ class BannerStats(models.Model):
     times_shown = models.IntegerField(default=0)
     times_clicked = models.IntegerField(default=0)
 
+    def __str__(self):
+        return "banner_id={}, shown={}, clicked={}".format(
+            self.banner_id, self.times_shown, self.times_clicked)
+
 
 class TeaserStats(models.Model):
     product = models.OneToOneField(Product)
     times_shown = models.IntegerField(default=0)
     times_clicked = models.IntegerField(default=0)
 
+    def __str__(self):
+        return "product_id={}, shown={}, clicked={}".format(
+            self.product_id, self.times_shown, self.times_clicked)
+
 
 class LogoStats(models.Model):
     merchant = models.OneToOneField(Merchant)
     times_shown = models.IntegerField(default=0)
     times_clicked = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "merchant_id={}, shown={}, clicked={}".format(
+            self.merchant_id, self.times_shown, self.times_clicked)
 
 
 class MerchantStats(models.Model):
@@ -28,3 +40,8 @@ class MerchantStats(models.Model):
     times_clicked = models.IntegerField(default=0)
     unique_visitors_shown = models.IntegerField(default=0)
     unique_visitors_clicked = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "merchant_id={}, shown={}, clicked={}, unique_shows={}, unique_clicks={}".format(
+            self.merchant_id, self.times_shown, self.times_clicked, self.unique_visitors_shown,
+            self.unique_visitors_clicked)
--- blackfriday/apps/reports/utils.py
+++ blackfriday/apps/reports/utils.py
@@ -20,24 +20,6 @@ class StatsUpdater:
         self.created_stats_number = 0
         self.updated_stats_number = 0
 
-    def process_batch_to_create(self):
-        self.stats_cls.objects.bulk_create(self.stats_to_create)
-        self.created_stats_number += len(self.stats_to_create)
-        self.stats_to_create = []
-
-    def process_batch_to_update(self):
-        bulk_update(self.stats_to_update)
-        self.updated_stats_number += len(self.stats_to_update)
-        self.stats_to_update = []
-
-    @staticmethod
-    def parse_logs(file_name):
-        counter = defaultdict(lambda: 0)
-        with open(file_name) as log_file:
-            for line in log_file:
-                counter[int(line.strip())] += 1
-        return counter
-
     def run(self, clicked_file, shown_file):
         shown_counter = self.parse_logs(shown_file)
         clicked_counter = self.parse_logs(clicked_file)
@@ -48,16 +30,13 @@ class StatsUpdater:
             times_clicked = clicked_counter[stats_id]
 
             if stats_id not in self.current_stats_ids:
-                stats_obj = self.stats_cls(
-                    times_clicked=times_clicked,
-                    times_shown=times_shown
-                )
+                stats_obj = self.stats_cls()
+                self.update_stats_obj(stats_obj, times_shown, times_clicked)
                 setattr(stats_obj, self.related_model_id_name, stats_id)
                 self.stats_to_create.append(stats_obj)
             else:
-                stats_obj = self.current_stats[stats_id]
-                stats_obj.times_shown = times_shown
-                stats_obj.times_clicked = times_clicked
+                stats_obj = self.self.current_stats[stats_id]
+                self.update_stats_obj(stats_obj, times_shown, times_clicked)
                 self.stats_to_update.append(stats_obj)
 
             if len(self.stats_to_create) > settings.BATCH_SIZE:
@@ -71,3 +50,55 @@ class StatsUpdater:
             self.process_batch_to_update()
 
         return self.created_stats_number, self.updated_stats_number
+
+    @staticmethod
+    def update_stats_obj(stats_obj, times_shown, times_clicked):
+        attr = {
+            'times_clicked': times_clicked,
+            'times_shown': times_shown
+        }
+        StatsUpdater.update_obj_attr(stats_obj, attr)
+
+    @staticmethod
+    def update_obj_attr(stats_obj, attr={}):
+        for k, v in attr.items():
+            setattr(stats_obj, k, v)
+
+    @staticmethod
+    def parse_logs(file_name):
+        counter = defaultdict(lambda: 0)
+        with open(file_name) as log_file:
+            for line in log_file:
+                counter[int(line.strip())] += 1
+        return counter
+
+    def process_batch_to_create(self):
+        self.stats_cls.objects.bulk_create(self.stats_to_create)
+        self.created_stats_number += len(self.stats_to_create)
+        self.stats_to_create = []
+
+    def process_batch_to_update(self):
+        bulk_update(self.stats_to_update)
+        self.updated_stats_number += len(self.stats_to_update)
+        self.stats_to_update = []
+
+
+class MerchantStatsUpdate(StatsUpdater):
+    @staticmethod
+    def parse_logs(file_name):
+        counter = defaultdict(list)
+        with open(file_name) as log_file:
+            for line in log_file:
+                merchant_id, client_id = map(int, line.split())
+                counter[merchant_id].append(client_id)
+        return counter
+
+    @staticmethod
+    def update_stats_obj(stats_obj, times_shown, times_clicked):
+        attr = {
+            'times_shown': len(times_shown),
+            'times_clicked': len(times_clicked),
+            'unique_visitors_shown': len(set(times_shown)),
+            'unique_visitors_clicked': len(set(times_clicked))
+        }
+        StatsUpdater.update_obj_attr(stats_obj, attr)
