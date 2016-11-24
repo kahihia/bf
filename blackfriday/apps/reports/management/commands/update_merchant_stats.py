d69ff0ff056cc9053b587c136407ee177afa60ae
Vladimir Shkoda <shkoda.vladimir@220-volt.ru>
2016-11-24 15:49:53 +0300

    BF-239 Added tests for the parsers

--- blackfriday/apps/reports/management/commands/update_merchant_stats.py
+++ blackfriday/apps/reports/management/commands/update_merchant_stats.py
@@ -1,11 +1,11 @@
-from apps.reports.utils import MerchantStatsUpdate
+from apps.reports.utils import MerchantStatsUpdater
 from apps.reports.models import MerchantStats
 
 from ._private import StatsUpdaterCommand
 
 
 class Command(StatsUpdaterCommand):
-    updater = MerchantStatsUpdate(
+    updater = MerchantStatsUpdater(
         stats_cls=MerchantStats,
         related_model_id_name='merchant_id'
     )
