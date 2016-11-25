46d6976c279006a5326cc32852700f3a22822c99
Vladimir Shkoda <shkoda.vladimir@220-volt.ru>
2016-11-25 16:48:02 +0300

    BF-239

--- blackfriday/apps/reports/management/commands/update_logo_stats.py
+++ blackfriday/apps/reports/management/commands/update_logo_stats.py
@@ -1,5 +1,6 @@
 from apps.reports.utils import StatsUpdater
 from apps.reports.models import LogoStats
+from apps.advertisers.models import Merchant
 
 from ._private import StatsUpdaterCommand
 
@@ -7,5 +8,6 @@ from ._private import StatsUpdaterCommand
 class Command(StatsUpdaterCommand):
     updater = StatsUpdater(
         stats_cls=LogoStats,
+        related_model_cls=Merchant,
         related_model_id_name='merchant_id'
     )
